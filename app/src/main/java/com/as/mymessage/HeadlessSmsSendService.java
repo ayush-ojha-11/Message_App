34bf1792b69245c5c5b9e7aa695fb8e29ce6d86d
ayush-ojha-11 <ayushojha1512@gmail.com>
2023-07-11 12:09:03 +0530

    3rd Commit Adding some files

---
+++ app/src/main/java/com/as/mymessage/HeadlessSmsSendService.java
@@ -0,0 +1,15 @@
+package com.as.mymessage;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class HeadlessSmsSendService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/as/mymessage/activities/ComposeSmsActivity.java
@@ -0,0 +1,16 @@
+package com.as.mymessage.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.as.mymessage.R;
+
+public class ComposeSmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_sms);
+    }
+}
